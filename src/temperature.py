9e526ce448a45cde95bc178ee9ad0c20c7074d05
Daniel Chen <Maveri2k@yahoo.com>
2015-06-26 09:33:26 -0500

    finished up rest of temp converts

--- src/temperature.py
+++ src/temperature.py
@@ -1,2 +1,11 @@
 def f_to_k(temp):
     return ((temp - 32) * (5 / 9.0)) + 273.15
+
+def k_to_c(temp):
+    return temp - 273.15
+
+def f_to_c(temp):
+    temp_k = f_to_k(temp)
+    temp = k_to_c(temp_k)
+    return temp
+
